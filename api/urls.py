1967a2d7d62a248cf6ca473842a3eb2190b3301f
sunny <sunny@jbcole.co.uk>
2025-01-12 14:43:31 +0000

    Created view and associated url, serializers and model methods to get invitation details

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
+from .views import invitation
+
+
 app_name = 'api'
 
-urlpatterns = []
+urlpatterns = [
+    path('invitation/<str:code>', invitation, name='invitation'),
+]
--- guests/models.py
+++ guests/models.py
@@ -1,8 +1,12 @@
 import uuid
+import random
+import string
 
 from model_utils.models import TimeStampedModel
 
 from django.db import models
+
+from data.constants import RANDOM_STRING_LENGTH
 
 
 # Create your models here.
@@ -14,6 +18,7 @@ class Invitation(TimeStampedModel):
     name = models.CharField(max_length=100)
     code = models.CharField(max_length=10, unique=True, blank=True, null=True)
     responded = models.BooleanField(default=False)
+    additional_info = models.TextField(blank=True, null=True)
 
     class Meta:
         verbose_name = 'Invitation'
@@ -21,6 +26,31 @@ class Invitation(TimeStampedModel):
 
     def __str__(self):
         return f'{self.name} - {self.responded}'
+
+    def save(self, *args, **kwargs):
+        """ Override save() to generate a unique code when creating a new Invitation """
+        code = self.generate_random_string()
+        while Invitation.objects.filter(code=code).exists():
+            code = self.generate_random_string()
+
+        self.code = code
+
+        super(Invitation, self).save(*args, **kwargs)
+
+    @staticmethod
+    def generate_random_string(length=RANDOM_STRING_LENGTH):
+        """ Generate a random alphanumeric string of a specified length """
+        return ''.join(random.choices(string.ascii_letters + string.digits, k=length)).lower()
+
+    @staticmethod
+    def get_invitation(code):
+        """ Get an invitation by code """
+        try:
+            invitation = Invitation.objects.get(code=code)
+        except Invitation.DoesNotExist:
+            invitation = None
+
+        return invitation
 
 
 class Guest(TimeStampedModel):
